349a136efa08d4d571985c98ed0ac649c4e1ee8e
Edwin Fuquen <efuquen@gmail.com>
2012-07-10 20:57:01 -0400

    completed the AkkaActorChannelHandler

--- src/main/scala/com/edftwin/snetty/AkkaActorChannelHandler.scala
+++ src/main/scala/com/edftwin/snetty/AkkaActorChannelHandler.scala
@@ -2,13 +2,45 @@ package com.edftwin.snetty
 
 import akka.actor.{Actor, ActorRef}
 
-import org.jboss.netty.channel.{ChannelHandlerContext, MessageEvent}
+import org.jboss.netty.channel.{
+  ChannelHandlerContext, MessageEvent, Channels,
+  SimpleChannelHandler, DownstreamMessageEvent}
 
-class AkkaActorChannelHandler(
-  getActor: (ChannelHandlerContext, MessageEvent) => ActorRef
-) extends Actor {
+case class AkkaChannelWrite[V](
+     data: V
+  )(
+    implicit val ctx: ChannelHandlerContext,
+    val e: MessageEvent
+  )
+
+  case class AkkaChannelRead[T](
+     data: T
+  )(
+    implicit val ctx: ChannelHandlerContext,
+    val e: MessageEvent
+  ) {
+    def toWrite[V](writeData: V) = AkkaChannelWrite[V](writeData)(ctx,e)
+  }
+
+class AkkaActorChannelHandler[T,V](
+  getActor: () => ActorRef
+) extends SimpleChannelHandler with Actor {
+
+  
 
   def receive = {
-    case _ => None
+    case write: AkkaChannelWrite[V] =>
+      val channel = write.e.getChannel
+      val channelFuture = Channels.future(channel)
+      val responseEvent = new DownstreamMessageEvent(
+        channel, channelFuture, write.data, channel.getRemoteAddress
+      )
+      write.ctx.sendDownstream(responseEvent)
+  }
+
+  override def messageReceived(ctx: ChannelHandlerContext, e: MessageEvent) {
+    val actor = getActor()
+    actor ! AkkaChannelRead[T](e.getMessage.asInstanceOf[T])(ctx, e)
+    super.messageReceived(ctx, e)
   }
 }
--- src/main/scala/com/edftwin/snetty/Main.scala
+++ src/main/scala/com/edftwin/snetty/Main.scala
@@ -2,6 +2,45 @@ package com.edftwin.snetty
 
 import akka.actor._
 
+import org.jboss.netty.channel.{
+  ChannelPipelineFactory, ChannelPipeline, Channels}
+
+import org.jboss.netty.handler.codec.string.{
+  StringDecoder, StringEncoder}
+import org.jboss.netty.handler.codec.frame.{
+  DelimiterBasedFrameDecoder, Delimiters}
+
+import org.jboss.netty.util.CharsetUtil
+
+class HelloActor extends Actor {
+  def receive = {
+    case read: AkkaChannelRead[String] =>
+      sender ! read.toWrite[String]("Hello " + read.data  + "!")
+  }
+}
+
 object Main extends App {
   val system = ActorSystem("Snetty")
+  val helloHandler = new AkkaActorChannelHandler[String,String](
+    () => system.actorOf(Props[HelloActor])
+  )
+
+  val piplineFactory = new ChannelPipelineFactory {
+    override def getPipeline: ChannelPipeline = {
+      val pipeline = Channels.pipeline
+
+      // Decoders
+      pipeline.addLast(
+        "frameDecoder",
+        new DelimiterBasedFrameDecoder(80, Array(Delimiters.lineDelimiter))
+      )
+      pipeline.addLast("stringDecoder", new StringDecoder(CharsetUtil.UTF_8))
+
+      // Encoder
+      pipeline.addLast("stringEncoder", new StringEncoder(CharsetUtil.UTF_8))
+      pipeline.addLast("HelloHandler", helloHandler)
+
+      pipeline
+    }
+  }
 }
